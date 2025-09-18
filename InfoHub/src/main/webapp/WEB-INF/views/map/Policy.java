26165901fb02c13fb7c46101c030a2e86c5ab74f
rang <dntb7558@naver.com>
2025-09-18 17:36:07 +0900

    도별정책 조회, 매핑

---
+++ InfoHub/src/main/webapp/WEB-INF/views/map/Policy.java
@@ -0,0 +1,20 @@
+package map;
+
+public class Policy {
+    private String title;
+    private String content;
+
+    public Policy(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
+
---
+++ InfoHub/src/main/webapp/WEB-INF/views/map/showMap.java
@@ -0,0 +1,20 @@
+package map;
+
+import java.security.Policy;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+
+public class showMap {
+    public static void main(String[] args) {
+        PolicyDAO dao = new PolicyDAO();
+        try {
+            dao.findPolicies(1, new int[]{1, 2, 3});
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
