712fbd56c09ea1d1d8d8e5b90ab3eae624274814
hassa <ahs6360@gamil.com>
2025-09-19 18:42:47 +0900

    seojin push

---
+++ InfoHub/src/main/java/com/infohub/project/registration/RegistrationDAO.java
@@ -0,0 +1,11 @@
+package com.infohub.project.registration;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RegistrationDAO {
+	List<RegistrationDTO> registrationlistAll() throws Exception;
+
+}
