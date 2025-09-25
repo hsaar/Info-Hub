667bed6ae728b208e3bc451c4dd8b599f0f440d2
rang <dntb7558@naver.com>
2025-09-25 18:12:00 +0900

    파일 전체 수정

--- InfoHub/src/main/java/com/infohub/project/articlecategories/articlecategoriesService.java
+++
@@ -1,7 +0,0 @@
-package com.infohub.project.articlecategories;
-
-import java.util.List;
-
-public interface articlecategoriesService {
-	List<ArticlecategoriesVO> articlecategorieslistAll() throws Exception;
-}
--- InfoHub/src/main/java/com/infohub/project/registration/RegistrationService.java
+++
@@ -1,9 +0,0 @@
-package com.infohub.project.registration;
-
-import java.util.List;
-
-public interface RegistrationService {
-	List<RegistrationDTO> registrationlistAll() throws Exception;
-}
-
-
