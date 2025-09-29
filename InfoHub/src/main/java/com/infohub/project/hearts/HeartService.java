2ad22a13c64b8f43f7a7cc725dd238d41802a6ac
rang <dntb7558@naver.com>
2025-09-29 09:30:24 +0900

    Hearts Impl 수정

--- InfoHub/src/main/java/com/infohub/project/hearts/HeartService.java
+++ InfoHub/src/main/java/com/infohub/project/hearts/HeartService.java
@@ -1,29 +1,16 @@
 package com.infohub.project.hearts;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.sql.SQLException;
 import java.util.List;
 
-@Service   // 서비스 계층 등록
-public class HeartService {
-
-    @Autowired
-    private HeartDAO dao;
+public interface HeartService {
 
     // 기사 좋아요 추가
-    public void addHeart(int loginNo, int articleId) throws SQLException {
-        dao.addHeart(loginNo, articleId);
-    }
+    void addHeart(int loginNo, int articleId) throws SQLException;
 
     // 내가 좋아요 누른 기사 목록
-    public List<HeartDTO> getHeartsByUser(int loginNo) throws SQLException {
-        return dao.getHeartsByUser(loginNo);
-    }
+    List<HeartDTO> getHeartsByUser(int loginNo) throws SQLException;
 
     // 좋아요 취소
-    public void deleteHeart(int heartNo) throws SQLException {
-        dao.deleteHeart(heartNo);
-    }
+    void deleteHeart(int heartNo) throws SQLException;
 }
