cf64e4ecceb730b852092fc3811286e1dc939f10
rang <dntb7558@naver.com>
2025-09-29 09:33:40 +0900

    Myboard Impl 수정

--- InfoHub/src/main/java/com/infohub/project/myboard/MyBoardService.java
+++ InfoHub/src/main/java/com/infohub/project/myboard/MyBoardService.java
@@ -1,34 +1,19 @@
 package com.infohub.project.myboard;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.sql.SQLException;
 import java.util.List;
 
-@Service   // 스프링 서비스 등록
-public class MyBoardService {
-
-    @Autowired
-    private MyBoardDAO dao;
+public interface MyBoardService {
 
     // 내가 쓴 게시글 목록 조회
-    public List<MyBoardDTO> getMyBoards(int loginNo) throws SQLException {
-        return dao.findBoardsByUser(loginNo);
-    }
+    List<MyBoardDTO> getMyBoards(int loginNo) throws SQLException;
 
     // 게시글 상세 조회
-    public MyBoardDTO getBoardDetail(int boardNo, int loginNo) throws SQLException {
-        return dao.findBoardDetail(boardNo, loginNo);
-    }
+    MyBoardDTO getBoardDetail(int boardNo, int loginNo) throws SQLException;
 
     // 게시글 수정
-    public void updateBoard(MyBoardDTO dto) throws SQLException {
-        dao.updateBoard(dto);
-    }
+    void updateBoard(MyBoardDTO dto) throws SQLException;
 
     // 게시글 삭제
-    public void deleteBoard(int boardNo, int loginNo) throws SQLException {
-        dao.deleteBoard(boardNo, loginNo);
-    }
+    void deleteBoard(int boardNo, int loginNo) throws SQLException;
 }
